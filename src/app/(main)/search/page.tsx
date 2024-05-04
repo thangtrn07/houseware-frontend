cfa4cd83256d0e9b57393673ae420ef56880861a
thangtrn <thangtrn01@gmail.com>
2024-05-04 09:40:03 +0700

    pls

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,19 +1,24 @@
-'use client';
-import React from 'react';
-import Breadcrumb from '../_components/Breadcrumb';
-import ProductList from './_components/ProductList';
-import Fillterbar from './_components/Fillterbar';
+"use client";
+import React, { Suspense } from "react";
+import Breadcrumb from "../_components/Breadcrumb";
+import ProductList from "./_components/ProductList";
+import Fillterbar from "./_components/Fillterbar";
+import LoadingState from "~/components/LoadingState";
 
 const SearchPage = () => {
    return (
       <>
-         <Breadcrumb data={['Tìm kiếm']} className='mb-5' />
-         <div className='flex flex-col gap-2 md:flex-row'>
-            <nav className='md:basis-2/12'>
-               <Fillterbar />
+         <Breadcrumb data={["Tìm kiếm"]} className="mb-5" />
+         <div className="flex flex-col gap-2 md:flex-row">
+            <nav className="md:basis-2/12">
+               <Suspense fallback={<LoadingState />}>
+                  <Fillterbar />
+               </Suspense>
             </nav>
-            <div className='md:basis-10/12'>
-               <ProductList />
+            <div className="md:basis-10/12">
+               <Suspense fallback={<LoadingState />}>
+                  <ProductList />
+               </Suspense>
             </div>
          </div>
       </>
