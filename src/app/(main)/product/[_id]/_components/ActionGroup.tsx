3572b03e57804a5efc6990071f23109722dcb259
thangtrn <thangtrn01@gmail.com>
2024-05-04 18:09:08 +0700

    change

--- src/app/(main)/product/[_id]/_components/ActionGroup.tsx
+++ src/app/(main)/product/[_id]/_components/ActionGroup.tsx
@@ -1,37 +1,40 @@
-'use client';
-import { Button } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
-import React from 'react';
-import { toast } from 'react-toastify';
-import { IProduct } from '~/interfaces/schema.interfaces';
-import useStores from '~/stores/stores';
+"use client";
+import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { toast } from "react-toastify";
+import { IProduct } from "~/interfaces/schema.interfaces";
+import useStores from "~/stores/stores";
 
 const ActionGroup = ({ data }: { data: IProduct }) => {
    const router = useRouter();
-   const { addToCart } = useStores();
+   const { user, addToCart } = useStores();
 
    const handleAddToCart = () => {
+      if (!user) {
+         return toast.warning("Vui lòng đăng nhập để thực hiện tao tác này.");
+      }
       addToCart(data);
-      toast.success('Đã thêm sản phẩm vào giỏ hàng');
+      toast.success("Đã thêm sản phẩm vào giỏ hàng");
    };
 
    const handleBuyNow = () => {
       handleAddToCart();
-      router.push('/cart');
+      router.push("/cart");
    };
 
    return (
-      <div className='flex gap-3'>
+      <div className="flex gap-3">
          <Button
-            variant='bordered'
-            className='border-1 w-full max-w-40 rounded border-[--blue-color] text-[--blue-color]'
+            variant="bordered"
+            className="border-1 w-full max-w-40 rounded border-[--blue-color] text-[--blue-color]"
             onClick={handleAddToCart}
          >
             Thêm vào giỏ hàng
          </Button>
          <Button
-            color='primary'
-            className='w-full max-w-40 rounded bg-[--red-color]'
+            color="primary"
+            className="w-full max-w-40 rounded bg-[--red-color]"
             onClick={handleBuyNow}
          >
             Mua ngay
