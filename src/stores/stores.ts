994ff18b36c5f8981f50365b104d9e8ac2f9e143
thangtrn <thangtrn01@gmail.com>
2024-05-04 09:26:53 +0700

    123123

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -1,16 +1,16 @@
-import { create } from 'zustand';
-import { persist, devtools } from 'zustand/middleware';
+import { create } from "zustand";
+import { persist, devtools } from "zustand/middleware";
 
-import { AuthSlice, authSlice } from './authSlice';
-import { CartSlice, cartSlice } from './cartSlice';
+import { AuthSlice, authSlice } from "./authSlice";
+import { CartSlice, cartSlice } from "./cartSlice";
 
-const useStores = create<AuthSlice & CartSlice>(
+const useStores = create<AuthSlice & CartSlice, any>(
    persist(
       devtools((...a) => ({
          ...authSlice(...a),
-         ...cartSlice(...a)
+         ...cartSlice(...a),
       })),
-      { name: 'cart-storage', partialize: (state) => ({ cart: state.cart }) }
+      { name: "cart-storage", partialize: (state) => ({ cart: state.cart }) }
    )
 );
 
